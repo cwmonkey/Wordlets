b1e7132fdb979ff19ab2e33f4c1ae552fcbafa91
cwmonkey <wackoburns@hotmail.com>
2014-05-23 11:26:04 -0700

    Moving jQuery

--- demo/app/templates/wrapper.tpl.php
+++ demo/app/templates/wrapper.tpl.php
@@ -42,7 +42,7 @@
 </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="vendor/jquery/jquery-1.7.2.min.js"><\/script>')</script>
 <? foreach ( $scripts as $script ): ?>
 	<script src="<?=$script ?>"></script>
 <? endforeach ?>
